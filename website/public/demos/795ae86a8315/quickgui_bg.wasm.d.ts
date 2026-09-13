bf13cc94cddc764595ce0210048ab6b915594cbb
EGOIST <hi@egoist.dev>
2026-09-13 10:56:44 +0800

    Rebuild the docs WASM demo with WebGL2 enabled.
    The hashed bundle now includes wgpu's WebGL backend so non-WebGPU
    browsers can load the gallery instead of the previous WebGPU-only build.

---
+++ website/public/demos/795ae86a8315/quickgui_bg.wasm.d.ts
@@ -0,0 +1,29 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: (a: any, b: number, c: number, d: number, e: number) => any;
+export const wasm_bindgen__convert__closures_____invoke__h1266c7452e398f41: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h1266c7452e398f41_14: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h1266c7452e398f41_15: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h5189da85cfeaa0e8: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h771b1a0a08855e1d: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__hdb6c00cf1bd88a0e: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h05db3ee0ab12343d: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h05db3ee0ab12343d_10: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h05db3ee0ab12343d_11: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h05db3ee0ab12343d_12: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h05db3ee0ab12343d_13: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h05db3ee0ab12343d_5: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h05db3ee0ab12343d_6: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h05db3ee0ab12343d_7: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h76c666956f59eb73: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__hd49719dea1c32150: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
